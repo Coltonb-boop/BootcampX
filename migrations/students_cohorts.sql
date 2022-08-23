5400f09b851a92bad4db0b0c54937afdb0d438d5
Coltonb-boop <coltonbitz@gmail.com>
2022-08-23 08:31:31 -0600

    added teachers and assistance_requests tables

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -15,12 +15,3 @@ CREATE TABLE students (
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
 );
-
--- SELECT students.name as student_name, email, cohorts.name as cohort_name
--- FROM students JOIN cohorts ON cohort_id = cohorts.id;
-
--- SELECT students.name, students.start_date AS student_start_date, cohorts.name, cohorts.start_date as cohort_start_date
--- FROM students 
--- JOIN cohorts ON cohort_id = cohorts.id
--- WHERE cohorts.start_date != students.start_date
--- ORDER BY cohort_start_date;
