55c585868d53341b34fe42ea1e2e507052c0e1fa
Coltonb-boop <coltonbitz@gmail.com>
2022-08-24 18:02:43 -0600

    Completed first steps in node postgres

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -12,15 +12,17 @@ const pool = new Pool({
 const input = process.argv.slice(2);
 
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
+SELECT DISTINCT teachers.name as teachers_name, cohorts.name as cohorts_name
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE ('%${input[0]}%')
-LIMIT ${input[1]};
+WHERE cohorts.name = '${input[0]}'
+ORDER BY teachers.name;
 `)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.cohorts_name}: ${user.teachers_name}`);
   })
 })
 .catch(err => console.error('query error', err.stack));
